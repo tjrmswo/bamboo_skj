f1327fa98d2829e3bd5ba5a7038a342560288f55
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-09-24 13:40:17 +0900

    chore: husky 적용

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['fix', 'feature', 'refactor', 'chore']],
+  },
 };
 
 export default nextConfig;
