5698170fd1bf0058970d9131594040053b4a7700
J_Keun <93309061+tjrmswo@users.noreply.github.com>
2024-12-15 22:34:58 +0900

    Fix/#44 (#46)
    * fix: 불필요한 주석 삭제
    
    * fix: 사용하지 않는 변수 제거
    
    * fix: config를 이용한 cors 에러 해결 시도

--- next.config.mjs
+++ next.config.mjs
@@ -3,19 +3,29 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-  // eslint: {
-  //   // Warning: This allows production builds to successfully complete even if
-  //   // your project has ESLint errors.
-  //   ignoreDuringBuilds: true,
-  // },
   reactStrictMode: true,
   swcMinify: true,
-  rewrites: async () => [
-    {
-      source: '/api/:path*',
-      destination: 'https://bamboo-skj.vercel.app/api/:path*',
-    },
-  ],
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
