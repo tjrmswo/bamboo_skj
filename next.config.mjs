4d46379f248f0f9974792ea74fdd65d4553ce2d7
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-12-15 22:57:55 +0900

    fix: cors 해결 - middleware 설정

--- next.config.mjs
+++ next.config.mjs
@@ -9,20 +9,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, PUT, DELETE, OPTIONS',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Content-Type, Authorization',
-          },
-        ],
+        destination: 'https://bamboo-skj.vercel.app/api/:path*',
       },
     ];
   },
---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server';
+
+export function middleware() {
+  const response = NextResponse.next();
+
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTIONS'
+  );
+  response.headers.set(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization'
+  );
+
+  return response;
+}
