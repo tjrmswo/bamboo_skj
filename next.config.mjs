3af2feb6620f488faf42e6f0e1fdb0ff76aca5c3
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2025-01-08 17:51:12 +0900

    fix: 배포 시 렌더링 안 되는 문제 해결중

--- next.config.mjs
+++ next.config.mjs
@@ -5,14 +5,14 @@ const nextConfig = {
   },
   reactStrictMode: true,
   swcMinify: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://bamboo-skj.vercel.app/api/:path*',
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/socket/io', // 클라이언트 요청 경로
+  //       destination: 'https://flchatserver.site/api/socket/io', // 실제 요청할 서버 주소
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
